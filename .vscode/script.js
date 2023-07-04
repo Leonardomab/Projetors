12a1e2c891be41bcd71a389fe24f5af0d4f03230
mabiga <leonardo.flvt@gmail.com>
2023-07-04 11:58:35 -0300

    ?

--- .vscode/script.js
+++ .vscode/script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // subistituir a igamem
   if (html.classList.contains("light")) {
     // se iver light mode adicionar imagem light
-    img.seletAtribute("scr", "avatar-light.png")
+    img.seletAtribute("scr", "avatar.png")
   } else {
     // se tiver sem light mode deixar imagem normal
-    img.setAttribute("scr", "avatar.png")
+    img.setAttribute("scr", "avatar-light.png")
   }
 }
