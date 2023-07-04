1815b1658cb070bce4e1aef9bcba0bbf8b1af0a1
mabiga <leonardo.flvt@gmail.com>
2023-07-04 11:52:08 -0300

    change image light dark mode

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if(html.classList.contains("light")){
-  //   html.classList.remove("light")
-  // } else{
-  //   html.classList.add("light")
-  // }
   html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("profile image")
+
+  // subistituir a igamem
+  if (html.classList.contains("light")) {
+    // se iver light mode adicionar imagem light
+    img.seletAtribute("scr", "avatar-light.png")
+  } else {
+    // se tiver sem light mode deixar imagem normal
+    img.setAttribute("scr", "avatar.png")
+  }
 }
